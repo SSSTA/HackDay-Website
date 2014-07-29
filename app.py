002b4354ddf7678077259bb476fb30ffb2506a4e
Eizo <eizoassik@gmail.com>
2014-07-29 11:02:55 +0800

    fix redirection

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 #encoding=utf-8
-from flask import Flask, request, render_template
+from flask import Flask, request, session, jsonify, render_template, redirect, url_for
 from pymongo import MongoClient
+from json import dumps, loads
+from utils import check_form, key_name
 
 app = Flask(__name__)
+# $md5sum <<< "SSSTA Hackday secret key"
+app.secret_key = "9e38117ea7ae309ff41dc1dd"
 mongo = MongoClient()
 hackday_db = mongo.hackday
+form_keys = {"name", "email", "phone", "team", "skill", "description"}
 
 
 @app.route('/hackers.html')
@@ -22,13 +27,50 @@ def showtime():
 
 
 @app.route('/commit.post', methods=["POST"])
-def handle_apply():
-    pass
+def commit_post():
+    commit_form = {key: value[0] for key, value in dict(request.form).items()}
+    print(commit_form)
+    new_form, err_key = check_form(commit_form)
+    print(new_form)
+    if new_form:
+        session['commit_form'] = dumps(new_form)
+        return render_template('confirm.tpl', info=new_form, key_name=key_name)
+    else:
+        return render_template('common_error.tpl',
+                               title="%s填写有误" % err_key,
+                               text="那个，这个表有那么难填...吗？")
 
 
-@app.route('/confirm.html', methods=["GET", "POST"])
-def handle_confirm():
-    pass
+@app.route('/register.get', methods=["GET"])
+def register_get():
+    if "commit_form" in session:
+        try:
+            hacker = loads(session["commit_form"])
+            hackday_db.hacker.insert(hacker)
+        except Exception:
+            print("DB-ERROR")
+            return render_template('common_error.tpl',
+                                   title="提交注册时发生了系统异常囧rz",
+                                   text="But WTF...?!")
+        else:
+            try:
+                session.pop("commit_form", None)
+            except Exception as ignored:
+                print("success but session.pop failed.")
+            else:
+                print("success.")
+            finally:
+                return redirect('/hackers.html', 301)
+    else:
+        return redirect('/apply.html', 301)
+
+
+@app.route('/history.get', methods=["GET"])
+def history_get():
+    if "commit_form" in session:
+        return jsonify(avaliable=True, **session['commit_form'])
+    else:
+        return jsonify(avaliable=False)
 
 
 def main():
